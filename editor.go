3ba0c418f454cec8f79f2a2f1a23e37e5fc45ffa
jeadie <jackeadie@duck.com>
2022-09-05 21:42:13 +1000

    add raw mode to editor  and basic window size commands

--- editor.go
+++ editor.go
@@ -3,23 +3,51 @@ package main
 import (
 	"fmt"
 	"golang.org/x/sys/unix"
+	"os"
 )
 
 type Editor struct {
-	originalTermios unix.Termios
+	originalTermios *unix.Termios
+	wRows, wCols    uint
+}
+
+func (e Editor) GetWindowSize() (uint, uint) {
+	if e.wRows != 0 && e.wCols != 0 {
+		return e.wRows, e.wCols
+	}
+	return e.getWindowSize()
+}
+
+// getWindowSize returns number of rows, then columns. (0, 0) if error occurs
+func (e Editor) getWindowSize() (uint, uint) {
+	ws, err := unix.IoctlGetWinsize(int(os.Stdin.Fd()), unix.TIOCGWINSZ)
+	if err != nil {
+		return 0, 0
+	}
+	e.wRows = uint(ws.Row)
+	e.wCols = uint(ws.Col)
+	return e.wRows, e.wCols
 }
 
 func (e Editor) DrawRows() {
-	for i := 0; i < 24; i++ {
-		fmt.Println("~\r")
+	r, _ := e.GetWindowSize()
+
+	fmt.Printf("\x1b[K") // Clear line
+
+	for i := uint(1); i < r; i++ {
+		fmt.Printf("~\r\n")
 	}
+	fmt.Printf("~\r")
 }
 
 func (e Editor) RefreshScreen() {
-	fmt.Printf("\x1b[2J") // Clear the screen
-	fmt.Printf("\x1b[H")  // Reposition Cursor
+	//fmt.Printf("\x1b[2J") // Clear the screen
+	fmt.Printf("\x1b[H")    // Reposition Cursor
+	fmt.Printf("\x1b[?25l") // Turn off cursor
 
 	e.DrawRows()
+
+	fmt.Printf("\x1b[?25h") // Turn on Cursor
 	fmt.Printf("\x1b[H")
 }
 
@@ -33,4 +61,52 @@ func (e Editor) KeyPress(x byte) bool {
 		fmt.Printf(string(x))
 	}
 	return false
+}
+
+func (e Editor) DisableRawMode() {
+	err := unix.IoctlSetTermios(int(os.Stdin.Fd()), unix.TIOCSETA, e.originalTermios)
+	if err != nil {
+		fmt.Printf(fmt.Errorf(
+			"Error on terminal close when disabling raw mode. Error: %w\n", err,
+		).Error(),
+		)
+	}
+}
+
+func EnableRawMode() (unix.Termios, error) {
+	// TODO: these are only for Mac OS, and not other linux
+	const ioctlReadTermios = unix.TIOCGETA
+	const ioctlWriteTermios = unix.TIOCSETA
+
+	fd := int(os.Stdin.Fd())
+
+	termios, err := unix.IoctlGetTermios(fd, ioctlReadTermios)
+	returnTermios := *termios
+
+	if err != nil {
+		return returnTermios, err
+	}
+
+	termios.Iflag &^= unix.IGNBRK | unix.BRKINT | unix.PARMRK | unix.ISTRIP | unix.INLCR | unix.IGNCR | unix.IXON
+	termios.Iflag &^= unix.IXON  // Ctrl-S and Ctrl-Q
+	termios.Iflag &^= unix.ICRNL // Ctrl-M
+
+	termios.Oflag &^= unix.OPOST // #Output Processing
+
+	termios.Lflag &^= unix.ECHO | unix.ECHONL // Echo
+	termios.Lflag &^= unix.ICANON             // Canonical Mode
+	termios.Lflag &^= unix.ISIG               // Ctrl-C and Ctrl-Z
+	termios.Lflag &^= unix.IEXTEN             // ctrl-V, ctrl-O (on macOS
+
+	termios.Cflag &^= unix.CSIZE | unix.PARENB
+	termios.Cflag |= unix.CS8
+
+	termios.Cc[unix.VMIN] = 0  // Number of bytes to let Read() return
+	termios.Cc[unix.VTIME] = 1 // Maximum wait time for Read(). Measured in tenths of a second
+
+	if err := unix.IoctlSetTermios(fd, ioctlWriteTermios, termios); err != nil {
+		return returnTermios, err
+	}
+
+	return returnTermios, err
 }
--- gram.go
+++ gram.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/sys/unix"
 	"os"
 )
 
 func main() {
 	c := make([]byte, 1)
 	cs := 1 // Number of characters read. Must be > 0 for initial for-loop check
-	var err error
 
+	e := Editor{}
 	t, err := EnableRawMode()
-	defer DisableRawMode(t)
+
+	e.originalTermios = &t
+	defer e.DisableRawMode()
 	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
-
-	e := Editor{originalTermios: t}
 
 	for true {
 		cs, _ = os.Stdin.Read(c)
@@ -36,53 +36,6 @@ func main() {
 
 func Ctrl(b byte) byte {
 	return b & 0x1f
-}
-
-func DisableRawMode(t unix.Termios) {
-	err := unix.IoctlSetTermios(int(os.Stdin.Fd()), unix.TIOCSETA, &t)
-	if err != nil {
-		fmt.Printf(fmt.Errorf(
-			"Error on terminal close when disabling raw mode. Error: %w\n", err,
-		).Error(),
-		)
-	}
-}
-
-func EnableRawMode() (unix.Termios, error) {
-	// TODO: these are only for Mac OS, and not other linux
-	const ioctlReadTermios = unix.TIOCGETA
-	const ioctlWriteTermios = unix.TIOCSETA
-
-	fd := int(os.Stdin.Fd())
-
-	termios, err := unix.IoctlGetTermios(fd, ioctlReadTermios)
-	returnT := *termios
-	if err != nil {
-		return returnT, err
-	}
-
-	termios.Iflag &^= unix.IGNBRK | unix.BRKINT | unix.PARMRK | unix.ISTRIP | unix.INLCR | unix.IGNCR | unix.IXON
-	termios.Iflag &^= unix.IXON  // Ctrl-S and Ctrl-Q
-	termios.Iflag &^= unix.ICRNL // Ctrl-M
-
-	termios.Oflag &^= unix.OPOST // #Output Processing
-
-	termios.Lflag &^= unix.ECHO | unix.ECHONL // Echo
-	termios.Lflag &^= unix.ICANON             // Canonical Mode
-	termios.Lflag &^= unix.ISIG               // Ctrl-C and Ctrl-Z
-	termios.Lflag &^= unix.IEXTEN             // ctrl-V, ctrl-O (on macOS
-
-	termios.Cflag &^= unix.CSIZE | unix.PARENB
-	termios.Cflag |= unix.CS8
-
-	termios.Cc[unix.VMIN] = 0  // Number of bytes to let Read() return
-	termios.Cc[unix.VTIME] = 1 // Maximum wait time for Read(). Measured in tenths of a second
-
-	if err := unix.IoctlSetTermios(fd, ioctlWriteTermios, termios); err != nil {
-		return returnT, err
-	}
-
-	return returnT, err
 }
 
 func isControlChar(x byte) bool {
