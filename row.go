54d88fdbd8ea19e7d68d7e97244fdb855fee8d42
jeadie <jackeadie@duck.com>
2022-09-09 09:20:59 +1000

    change Row struct to handle \t and setting chars

--- row.go
+++ row.go
@@ -1,26 +1,40 @@
 package main
 
-import "strings"
+import (
+	"strings"
+)
 
 type Row struct {
-	render string
-	src    string
+	src []byte
 }
 
 func ConstructRow(s string) Row {
-	render := strings.ReplaceAll(s, "\t", "    ")
-
 	return Row{
-		render: render,
-		src:    s,
+		src: []byte(s),
 	}
 }
 
 func (r Row) Render() string {
-	return r.render
+	srcStr := string(r.src)
+	return strings.ReplaceAll(srcStr, "\t", "    ")
+}
+
+func (r *Row) SetCharAt(renderI uint, b byte) {
+
+	// Convert render index to src index.
+	j := 0
+	for i := uint(0); i < renderI; i++ {
+		j++
+		if r.src[i] == '\t' {
+			i += 4
+		} else {
+			i++
+		}
+	}
+	r.src[j] = b
 }
 
 // RenderLen returns the length of the rendered row.
 func (r Row) RenderLen() uint {
-	return uint(len(r.render))
+	return uint(len(r.Render()))
 }
