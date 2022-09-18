46ae29e4abfc736d075b5f7b49265cade037741b
jeadie <jackeadie@duck.com>
2022-09-18 11:16:31 +1000

    refactor syntax to new file

--- highlighting.go
+++ highlighting.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"regexp"
 	"strings"
-	"unicode"
 )
 
 type Colour string
@@ -31,20 +29,6 @@ const (
 	White              = "\033[97m"
 )
 
-type LanguageSyntax struct {
-	exts        []string
-	keywords    []string
-	stringChars []byte
-	comment     string
-}
-
-var goSyntax = LanguageSyntax{
-	exts:        []string{".go"},
-	keywords:    []string{"uint", "import", "package", "const", "var", "func", "map", "string", "byte", "struct", "int", "any", "error", "type", "continue", "break", "append", "if", "len", "return", "else"},
-	comment:     "//",
-	stringChars: []byte{'"', '\'', '`'},
-}
-
 var AllowedColours = map[string]Colour{"Black": Black, "Default": Default, "Red": Red, "DarkRed": DarkRed, "Green": Green, "DarkGreen": DarkGreen, "DarkYellow": DarkYellow, "Orange": Orange, "Blue": Blue, "DarkBlue": DarkBlue, "Cyan": Cyan, "Magenta": Magenta, "DarkMagenta": DarkMagenta, "DarkCyan": DarkCyan, "LightGray": LightGray, "DarkGray": DarkGray, "White": White}
 
 // Cprintf is Printf with colours. Replace any `%d` with `%Red.d` and it will be printed in Red, or
@@ -57,148 +41,6 @@ func Cprintf(format string, a ...any) (n int, err error) {
 // C wraps a string with colour encoding characters. Resets colour at end of string.
 func C(s string, c Colour) string {
 	return fmt.Sprintf("%s%s%s", c, s, Reset)
-}
-
-// AllWordIndices returns all indices of a subword within a larger string.
-func AllWordIndices(s, sub string) []int {
-	r := make([]int, 0)
-
-	i := strings.Index(s, sub)
-	tot := 0
-
-	for i != -1 {
-		// TODO: ensure substring is subword before appending (but continue iteration)
-		if isWord(s, tot+i, tot+i+len(sub)) {
-			r = append(r, tot+i)
-		}
-
-		tot += len(sub) + i
-		i = strings.Index(s[tot:], sub)
-	}
-	return r
-}
-
-// isWord checks if the word s[start:end] is a standalone word (i.e. not within a word)
-func isWord(s string, start, end int) bool {
-	if start != 0 {
-		c := rune(s[start-1])
-		if unicode.IsLetter(c) || unicode.IsDigit(c) || c == '_' {
-			return false
-		}
-	}
-
-	if end < len(s) {
-		c := rune(s[end])
-		if unicode.IsLetter(c) || unicode.IsDigit(c) || c == '_' {
-			return false
-		}
-	}
-
-	return true
-}
-
-func SimpleGolangSyntax(s string) string {
-	hl := make([]Colour, len(s))
-
-	// Keyword highlights
-	for _, k := range goSyntax.keywords {
-		for _, idx := range AllWordIndices(s, k) {
-			for i := 0; i < len(k); i++ {
-				hl[idx+i] = Orange
-			}
-		}
-	}
-
-	// Comments
-	cIdx := strings.Index(s, goSyntax.comment)
-	if cIdx != -1 {
-		for i := cIdx; i < len(s); i++ {
-			hl[i] = DarkGray
-		}
-	}
-
-	// TODOs
-	toDoIdx := strings.Index(s, goSyntax.comment+" TODO")
-	if toDoIdx != -1 {
-		for i := toDoIdx + len(goSyntax.comment); i < len(s); i++ {
-			hl[i] = DarkYellow
-		}
-	}
-
-	// Numbers
-	HighlightRegex(s, "[-]?\\d[\\d,]*[\\.]?[\\d{2}]*", &hl, Blue)
-
-	// Strings
-	for _, b := range goSyntax.stringChars {
-		HighlightRegex(s, fmt.Sprintf("%s.*%s", string(b), string(b)), &hl, DarkGreen)
-	}
-
-	return ApplyColours(s, hl)
-}
-
-func HighlightRegex(s, regex string, hl *[]Colour, c Colour) {
-	re, _ := regexp.Compile(regex)
-	for _, idx := range re.FindAllIndex([]byte(s), -1) {
-		for i := idx[0]; i < idx[1]; i++ {
-			(*hl)[i] = c
-		}
-	}
-}
-
-// ApplyColours per character, onto a string.
-func ApplyColours(s string, hl []Colour) string {
-	if len(s) == 0 {
-		return ""
-	} else if len(s) != len(hl) {
-		return s
-	}
-
-	result := ""
-
-	currI := 0 // Start of current highlight segment
-	currC := hl[0]
-
-	for i, c := range hl {
-		// Highlighting has changed, apply previous
-		if c != currC {
-			//fmt.Println(currI, i, currC)
-			if currC != "" {
-				result += C(s[currI:i], currC)
-			} else {
-				result += s[currI:i]
-			}
-
-			currI = i
-			currC = c
-		}
-	}
-	if currI+1 != len(s) {
-		if currC != "" {
-			result += C(s[currI:], currC)
-		} else {
-			result += s[currI:]
-		}
-	}
-
-	return result
-}
-
-func applyGolangFuncSyntax(s string) string {
-	funcs := strings.Index(s, "func ")
-	if funcs == -1 || (funcs != 0 && s[funcs] != ' ') {
-		return s
-	}
-
-	funcColour := C("func ", Orange)
-	l := s[:funcs] + funcColour + s[funcs+5:]
-
-	startFunc := strings.Index(l, funcColour)
-	endFunc := strings.IndexByte(l, '(')
-	if endFunc == -1 {
-		return l
-	}
-	// Highlight from end of "func " to start of bracket (
-	return l[:startFunc+len(funcColour)] + C(l[startFunc+len(funcColour):endFunc], Green) + l[endFunc:] // + C(s[startFunc + 5:endFunc], Green)
 }
 
 func doCprintfParse(x string) string {
---
+++ syntax.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"unicode"
+)
+
+type LanguageSyntax struct {
+	exts        []string
+	keywords    []string
+	stringChars []byte
+	comment     string
+}
+
+var goSyntax = LanguageSyntax{
+	exts:        []string{".go"},
+	keywords:    []string{"uint", "import", "package", "const", "var", "func", "map", "string", "byte", "struct", "int", "any", "error", "type", "continue", "break", "append", "if", "len", "return", "else"},
+	comment:     "//",
+	stringChars: []byte{'"', '\'', '`'},
+}
+
+// AllWordIndices returns all indices of a subword within a larger string.
+func AllWordIndices(s, sub string) []int {
+	r := make([]int, 0)
+
+	i := strings.Index(s, sub)
+	tot := 0
+
+	for i != -1 {
+		// TODO: ensure substring is subword before appending (but continue iteration)
+		if isWord(s, tot+i, tot+i+len(sub)) {
+			r = append(r, tot+i)
+		}
+
+		tot += len(sub) + i
+		i = strings.Index(s[tot:], sub)
+	}
+	return r
+}
+
+// isWord checks if the word s[start:end] is a standalone word (i.e. not within a word)
+func isWord(s string, start, end int) bool {
+	if start != 0 {
+		c := rune(s[start-1])
+		if unicode.IsLetter(c) || unicode.IsDigit(c) || c == '_' {
+			return false
+		}
+	}
+
+	if end < len(s) {
+		c := rune(s[end])
+		if unicode.IsLetter(c) || unicode.IsDigit(c) || c == '_' {
+			return false
+		}
+	}
+
+	return true
+}
+
+func SimpleGolangSyntax(s string) string {
+	hl := make([]Colour, len(s))
+
+	// Keyword highlights
+	for _, k := range goSyntax.keywords {
+		for _, idx := range AllWordIndices(s, k) {
+			for i := 0; i < len(k); i++ {
+				hl[idx+i] = Orange
+			}
+		}
+	}
+
+	// Comments
+	cIdx := strings.Index(s, goSyntax.comment)
+	if cIdx != -1 {
+		for i := cIdx; i < len(s); i++ {
+			hl[i] = DarkGray
+		}
+	}
+
+	// TODOs
+	toDoIdx := strings.Index(s, goSyntax.comment+" TODO")
+	if toDoIdx != -1 {
+		for i := toDoIdx + len(goSyntax.comment); i < len(s); i++ {
+			hl[i] = DarkYellow
+		}
+	}
+
+	// Numbers
+	HighlightRegex(s, "[-]?\\d[\\d,]*[\\.]?[\\d{2}]*", &hl, Blue)
+
+	// Strings
+	for _, b := range goSyntax.stringChars {
+		HighlightRegex(s, fmt.Sprintf("%s.*%s", string(b), string(b)), &hl, DarkGreen)
+	}
+
+	return ApplyColours(s, hl)
+}
+
+func HighlightRegex(s, regex string, hl *[]Colour, c Colour) {
+	re, _ := regexp.Compile(regex)
+	for _, idx := range re.FindAllIndex([]byte(s), -1) {
+		for i := idx[0]; i < idx[1]; i++ {
+			(*hl)[i] = c
+		}
+	}
+}
+
+// ApplyColours per character, onto a string.
+func ApplyColours(s string, hl []Colour) string {
+	if len(s) == 0 {
+		return ""
+	} else if len(s) != len(hl) {
+		return s
+	}
+
+	result := ""
+
+	currI := 0 // Start of current highlight segment
+	currC := hl[0]
+
+	for i, c := range hl {
+		// Highlighting has changed, apply previous
+		if c != currC {
+			//fmt.Println(currI, i, currC)
+			if currC != "" {
+				result += C(s[currI:i], currC)
+			} else {
+				result += s[currI:i]
+			}
+
+			currI = i
+			currC = c
+		}
+	}
+	if currI+1 != len(s) {
+		if currC != "" {
+			result += C(s[currI:], currC)
+		} else {
+			result += s[currI:]
+		}
+	}
+
+	return result
+}
